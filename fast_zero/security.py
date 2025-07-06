dd6f3c6be7906dcbd1691a7ccca5f82c8297e756
Maria Coelho <mecoelhobrocha@outlook.com>
2025-07-06 14:10:43 -0300

    :recycle: refactor: creating routers for Users and Auth and moving constants to environment variables.

--- fast_zero/security.py
+++ fast_zero/security.py
@@ -11,23 +11,25 @@ from sqlalchemy.orm import Session
 
 from fast_zero.database import get_session
 from fast_zero.models import User
+from fast_zero.settings import Settings
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+settings = Settings()
 
-SECRET_KEY = "your-secret-key"  # Provisório
-ALGORITHM = "HS256"
-ACESS_TOKEN_EXPIRE_MINUTES = 30
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
+
 pwd_context = PasswordHash.recommended()
 
 
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.now(tz=ZoneInfo("UTC")) + timedelta(
-        minutes=ACESS_TOKEN_EXPIRE_MINUTES
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
     )
     to_encode.update({"exp": expire})
-    encode_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encode_jwt
+    encoded_jwt = encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
+    return encoded_jwt
 
 
 def get_password_hash(password: str):
@@ -49,7 +51,9 @@ def get_current_user(
     )
 
     try:
-        payload = decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = decode(
+            token, settings.SECRET_KEY,
+            algorithms=[settings.ALGORITHM])
         subject_email = payload.get("sub")
 
         if not subject_email:
