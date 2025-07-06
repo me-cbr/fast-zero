da8e4d3be9f8669586eee70f5762079db17aa4ac
Maria Coelho <mecoelhobrocha@outlook.com>
2025-07-06 15:33:27 -0300

    :sparkles: feat: implemented task endpoints

--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, ConfigDict, EmailStr, Field
 
+from fast_zero.models import TodoState
+
 
 class Message(BaseModel):
     message: str
@@ -30,3 +32,29 @@ class Token(BaseModel):
 class FilterPage(BaseModel):
     offset: int = Field(0, ge=0)
     limit: int = Field(100, ge=1)
+
+
+class TodoSchema(BaseModel):
+    title: str
+    description: str
+    state: TodoState
+
+
+class TodoPublic(TodoSchema):
+    id: int
+
+
+class TodoList(BaseModel):
+    todos: list[TodoPublic]
+
+
+class FilterTodo(FilterPage):
+    title: str | None = Field(None, min_length=3, max_length=20)
+    description: str | None = Field(None, min_length=3, max_length=20)
+    state: TodoState | None = None
+
+
+class TodoUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    state: TodoState | None = None
